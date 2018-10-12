9be8c87ed2fcc7aefa7aeb1e3af394666a4db2f3
amedennikova <amedennikova@lohika.com>
2018-10-12 18:00:34 +0300

    Phase4-driver

--- conftest.py
+++ conftest.py
@@ -1,11 +1,18 @@
 
 from selenium import webdriver
 import pytest
+import os, sys, platform
+
+
+os_name = platform.system().lower()
+root_dir = sys.path[0]
+extension = ".exe" if "win" in os_name else ""
+path_to_driver = os.path.join(root_dir, "driver", "chromedriver" + extension)
 
 
 @pytest.fixture(scope='class')
 def driver_setup(request):
-    web_driver = webdriver.Chrome(executable_path="./driver/chromedriver")
+    web_driver = webdriver.Chrome(executable_path="../driver/chromedriver")
     request.cls.driver = web_driver
     yield
     web_driver.close()
